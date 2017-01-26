de7c3886365e4d63914463df06292754c41fe0a1
myl <moyuli@sensetime.com>
2017-01-26 16:19:47 +0800

    Update hdfs reader

--- lib/spark_toolkit/hadoop/hdfs/reader.rb
+++ lib/spark_toolkit/hadoop/hdfs/reader.rb
@@ -1,59 +1,21 @@
-module SparkToolkit
-  module HDFS
-    class Reader
-      # ==== Parameters
-      #
-      # * input<~HdfsDataInputStream>
-      def initialize(is)
-        @is = is
-        reader = java.io.InputStreamReader.new(is)
-        @reader = java.io.BufferedReader.new(reader)
-      end
+class Java::OrgApacheHadoopHdfsClient::HdfsDataInputStream
+  def read(*args)
+    @io ||= self.to_io
+    @io.read(*args)
+  end
 
-      def to_io
-        @is.to_io
-      end
+  def readline
+    @io ||= self.to_io
+    @io.read_line
+  end
 
-      def readline
-        @reader.readLine
-      end
-
-      def readlines(rc=-1)
-        if rc < 0
-          return readalllines
-        end
-        lines = []
-        while rc > 0
-          line = readline
-          if line == nil
-            break
-          end
-          lines << line
-          rc -= 1
-        end
-        lines
-      end
-
-      def readalllines
-        lines = []
-        while true
-          line = readline
-          if line == nil
-            break
-          end
-          lines << line
-        end
-        lines
-      end
-
-      def close
-        @reader.close
-      end
-
-      def method_missing(method_name, *args)
-        @reader.send(method_name, *args)
-      end
+  def readlines
+    lines = []
+    loop do
+      line = readline
+      line.nil? ? break : lines << line
     end
+    lines
   end
 end
 
