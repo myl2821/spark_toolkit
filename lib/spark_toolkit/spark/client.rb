f76987e5eb597b10c887757ffc1cfa3345531465
myl <moyuli@sensetime.com>
2017-02-03 18:23:31 +0800

    Add Kerberos support into spark client

--- lib/spark_toolkit/spark/client.rb
+++ lib/spark_toolkit/spark/client.rb
@@ -1,8 +1,12 @@
 module SparkToolkit
   module Spark
     class Client
+      java_import org.apache.hadoop.security.UserGroupInformation
+      java_import org.apache.spark.deploy.SparkHadoopUtil
+
       def initialize(hconf)
         @hconf = hconf
+        UserGroupInformation.set_configuration(@hconf)
         @sconf = org.apache.spark.SparkConf.new
         @sconf.set_spark_home(ENV['SPARK_HOME']) if ENV['SPARK_HOME']
       end
@@ -20,14 +24,22 @@ module SparkToolkit
       # <~AppID>
       def yarn_submit(args)
         prepare_yarn_propreties
-        cli_args = org.apache.spark.deploy.yarn.ClientArguments.new(args)
+        begin
+          cli_args = org.apache.spark.deploy.yarn.ClientArguments.new(args)
+        rescue ArgumentError # Spark 1.x
+          cli_args = org.apache.spark.deploy.yarn.ClientArguments.new(args, @sconf)
+        end
         client = org.apache.spark.deploy.yarn.Client.new(cli_args, @hconf, @sconf)
         client.submit_application
       end
 
       def yarn_run(args)
         prepare_yarn_propreties
-        cli_args = org.apache.spark.deploy.yarn.ClientArguments.new(args)
+        begin
+          cli_args = org.apache.spark.deploy.yarn.ClientArguments.new(args)
+        rescue ArgumentError # Spark 1.x
+          cli_args = org.apache.spark.deploy.yarn.ClientArguments.new(args, @sconf)
+        end
         client = org.apache.spark.deploy.yarn.Client.new(cli_args, @hconf, @sconf)
         client.run
       end
@@ -51,14 +63,26 @@ module SparkToolkit
         end
       end
 
+      def active_kerberos
+        prepare_yarn_propreties
+
+        @sconf.set("spark.hadoop.hadoop.security.authentication", "kerberos")
+        @sconf.set("spark.hadoop.hadoop.security.authorization", "true")
+
+        UserGroupInformation.set_configuration(SparkHadoopUtil.get.newConfiguration(@sconf))
+        credentials = UserGroupInformation.getLoginUser.getCredentials
+        SparkHadoopUtil.get.addCurrentUserCredentials(credentials)
+      end
+
       private
       def prepare_yarn_propreties
         @sconf.set_master('yarn')
         begin
-          @sconf.get 'spark.submit.deployMode'
+          @sconf.get('spark.submit.deployMode')
         rescue
           @sconf.set('spark.submit.deployMode', 'cluster')
         end
+
         java.lang.System.setProperty("SPARK_YARN_MODE", "true")
       end
     end
